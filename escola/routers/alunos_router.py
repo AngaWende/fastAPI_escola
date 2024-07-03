fc2cfb9679a98c83a8a238612b1ee1b5a6b58af2
AngaWende <lucas.angawende@hotmail.com>
2024-07-03 16:55:21 -0300

    classes e atribuições separadas em arquivos distintos

---
+++ escola/routers/alunos_router.py
@@ -0,0 +1,54 @@
+import sqlite3
+
+from fastapi import APIRouter, Depends, HTTPException
+from pydantic import BaseModel
+
+router = APIRouter(prefix='/alunos')
+
+def get_db():
+    conn = sqlite3.connect("shared/escola.db")
+    try:
+        yield conn
+    finally:
+        conn.close()
+
+
+class AlunoResponse(BaseModel):
+    nome: str
+    cpf: str
+    email: str
+
+@router.get('/listar_alunos')
+def listar_alunos(conn: sqlite3.Connection = Depends(get_db)):
+    cursor = conn.cursor()
+    cursor.execute('select * from alunos')
+    rows = cursor.fetchall()
+    alunos = [{'id': row[0], 'nome': row[1], 'cpf': row[2], 'e_mail': row[3]} for row in rows]
+    return {'alunos': alunos}
+
+@router.post('/add_aluno')
+def add_aluno(aluno: AlunoResponse, conn: sqlite3.Connection = Depends(get_db)):
+    cursor = conn.cursor()
+    try:
+        dados = (aluno.nome, aluno.cpf, aluno.email)
+        cursor.execute('insert into alunos (nome, cpf, email) values (?,?,?)', dados)
+        aluno_id = cursor.lastrowid
+        conn.commit()
+        return {"id": aluno_id, **aluno.dict()}
+    except sqlite3.Error as e:
+        raise HTTPException(status_code=500, detail=f"Database error: {e}")
+    finally:
+        cursor.close()
+
+@router.delete('/excluir_aluno')
+def excluir_aluno(nome, conn: sqlite3.Connection = Depends(get_db)):
+    cursor = conn.cursor()
+    try:
+        dados = (nome,)
+        cursor.execute('delete from alunos where nome = (?)', dados)
+        conn.commit()
+        return {"Aluno excluído": nome}
+    except sqlite3.Error as e:
+        raise HTTPException(status_code=500, detail=f"Database error: {e}")
+    finally:
+        cursor.close()
